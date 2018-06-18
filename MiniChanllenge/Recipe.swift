632283b8de8132ccecaf3c30d5b3d7e47f71b3f4
henrique-a <henriquearaujoandrade@gmail.com>
2018-06-18 10:58:26 -0300

    Pegando imagem da receita da URL

--- MiniChanllenge/Recipe.swift
+++ MiniChanllenge/Recipe.swift
@@ -15,5 +15,28 @@ struct Recipe: Decodable {
     var howToPrepare: String
     var timeToPrepare: Int
     var portions: Int
-    var image: String
+    var imageURL: String
+    var image: UIImage
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case ingridients
+        case howToPrepare
+        case timeToPrepare
+        case portions
+        case imageURL = "image"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try values.decode(String.self, forKey: .name)
+        self.ingridients = try values.decode([Ingridient].self, forKey: .ingridients)
+        self.howToPrepare = try values.decode(String.self, forKey: .howToPrepare)
+        self.timeToPrepare = try values.decode(Int.self, forKey: .timeToPrepare)
+        self.portions = try values.decode(Int.self, forKey: .portions)
+        self.imageURL = try values.decode(String.self, forKey: .imageURL)
+        let url: URL = URL(string: imageURL)!
+        let imageData:NSData = try NSData(contentsOf: url)
+        self.image = UIImage(data: imageData as Data)!
+    }
 }
